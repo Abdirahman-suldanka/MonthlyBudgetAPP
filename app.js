146f740d66d1a9bf261a088d780b1ad349d479c5
abdirahman-suldanka <abdurahmansuldanka@gmail.com>
2026-01-05 11:52:09 +0300

    verions 10

--- app.js
+++ app.js
@@ -1,89 +1,82 @@
-const totalBudget = document.getElementById("totalBudget");
-const remainingBudget = document.getElementById("remainingBudget");
+// get all elements
 const budgetForm = document.getElementById("budgetForm");
 const budgetInput = document.getElementById("budgetInput");
-const expenseForm = document.getElementById("expenseForm");
-const expenseName = document.getElementById("expenseName");
-const expenseAmount = document.getElementById("expenseAmount");
-const expenseList = document.getElementById("expenseList");
-const alertMsg = document.getElementById("alert");
+const totalBudget = document.getElementById("totalBudget");
+const remainingBudget = document.getElementById("remainingBudget");
+const expForm = document.getElementById("expenseForm");
+const expName = document.getElementById("expenseName");
+const expAmount = document.getElementById("expenseAmount");
+const expList = document.getElementById("expenseList");
 const nameError = document.getElementById("nameError");
+const amountError = document.getElementById("alert");
 
 budget = 0;
 remaining = 0;
-amount = 0;
 
 function saveToLocal() {
   localStorage.setItem("budget", budget);
   localStorage.setItem("remaining", remaining);
-  localStorage.setItem("expenses", expenseList.innerHTML);
+  localStorage.setItem("allExpenses", expList.innerHTML);
 }
 
-//budget form event
 budgetForm.addEventListener("submit", (event) => {
   event.preventDefault();
   budget = Number(budgetInput.value);
   remaining = budget;
+  console.log(remaining);
 
   totalBudget.innerHTML = budget;
   remainingBudget.innerHTML = remaining;
   saveToLocal();
+
+  expList.innerHTML = "";
   budgetInput.value = "";
-  expenseList.innerHTML = "";
 });
 
-expenseForm.addEventListener("submit", (event) => {
+expForm.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  amount = Number(expenseAmount.value);
-  title = expenseName.value;
+  name = expName.value;
+  amount = Number(expAmount.value);
+  // console.log("name is:", name, "amount:", amount)
 
   if (amount > remaining) {
-    alertMsg.classList.remove("hidden");
+    amountError.classList.remove("hidden");
     return;
   }
 
-  alertMsg.classList.add("hidden");
+  amountError.classList.add("hidden");
   remaining -= amount;
   remainingBudget.innerHTML = remaining;
-  let displayDiv = document.createElement("div");
+
+  displayDiv = document.createElement("div");
   displayDiv.innerHTML = `
-  <span> ${title} </span>
-  <span> ${amount} </span>
+  <span> ${name} </span>
+  <span>${amount} </span>
   `;
   displayDiv.className =
-    "flex justify-between border border-red-300 bg-red-50 p-3 rounded-lg ";
-  expenseList.appendChild(displayDiv);
+    "flex justify-between border border-red-300 bg-red-50 p-3 rounded-lg";
+  expList.appendChild(displayDiv);
+
   saveToLocal();
-  expenseName.value = "";
-  expenseAmount.value = "";
+  expName.value = "";
+  expAmount.value = "";
 });
 
 window.addEventListener("DOMContentLoaded", () => {
   savedBudget = localStorage.getItem("budget");
-  savedRemaining = localStorage.getItem("remaining");
-  savedExpenses = localStorage.getItem("expenses");
+  savedRemaining = Number(localStorage.getItem("remaining"));
+  savedExpenses = localStorage.getItem("allExpenses");
 
   if (savedBudget) {
     totalBudget.innerHTML = savedBudget;
   }
+
   if (savedRemaining) {
     remainingBudget.innerHTML = savedRemaining;
+    console.log(typeof savedRemaining);
   }
   if (savedExpenses) {
-    expenseList.innerHTML = savedExpenses;
-  }
-});
-
-expenseName.addEventListener("input", (event) => {
-  expName = event.target.value;
-  if (
-    expName.length < 15 &&
-    !/[1-9]/.test(expName) &&
-    !/[^A-Za-z1-9]/.test(expName)
-  ) {
-    nameError.classList.add("hidden");
-  } else {
-    nameError.classList.remove("hidden");
+    expList.innerHTML = savedExpenses;
   }
 });
